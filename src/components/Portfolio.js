9dfb5bc8662546a792d78c29283563c49ce60352
Juan Camacho <kmilokmacho@hotmail.com>
2021-01-05 21:35:13 -0500

    added a map to shorten lines of code

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,169 +1,60 @@
 import React from "react";
-// --------------IMAGES ---------------------------
-import map from "../image/projectsicon/map.png";
-import calc from "../image/projectsicon/calc.png";
-import drumkit from "../image/projectsicon/drumkit.png";
-import generativeArt from "../image/projectsicon/generative_art.png";
-import gorjeo from "../image/projectsicon/gorjeo3.jpg";
-import interactiveparticles from "../image/projectsicon/interactiveparticles.gif";
-import rememberSeq from "../image/projectsicon/remember-seq.png";
-import spartikus from "../image/projectsicon/spartikus.png";
-import stooltracker from "../image/projectsicon/stooltracker.jpg";
-import tindog from "../image/projectsicon/tindog.png";
-import todos from "../image/projectsicon/todos.png";
-import weatherteller from "../image/projectsicon/weatherteller.jpg";
 // --------------FONT AWESOME IMPORTS --------------------
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearchPlus } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearchPlus } from "@fortawesome/free-solid-svg-icons";
 // REACT POPUPBOX
-import { PopupboxManager, popupboxContainer, PopupboxContainer } from 'react-popupbox';
-import 'react-popupbox/dist/react-popupbox.css'
-
-// make a function that loops thru array of pic to display
+import {
+  PopupboxManager,
+  popupboxContainer,
+  PopupboxContainer,
+} from "react-popupbox";
+import "react-popupbox/dist/react-popupbox.css";
+import projectsInfo from "../projectsInfo/projectsInfo";
 
 const Portfolio = () => {
-  const openPopupbox = () => {
-    const content = (
-      <>
-    <img className="portfolio-image-popupbox" src={map} alt="google map clone project"/>
-    <b>Go to Site</b> <a className="hyper-link" onClick={() => window.open("https://jcgooglemapsreplica.netlify.app/")}>hello</a>
-    </>
-    )
-    PopupboxManager.open({content})
-  }
-
   const popupboxConfig = {
     titleBar: {
-      enable: true,
-      text: "Google Maps Clone Project"
+      enable: false,
+      text: "Google Maps Clone Project",
     },
     fadeIn: true,
-    fadeInSpeed: 500
-  }
+    fadeInSpeed: 500,
+  };
+  const mappedProjects = projectsInfo.map((a, idx) => {
+    const openPopupbox = () => {
+      const content = (
+        <>
+            <b>{a.desc}</b>{" "}
+            <hr/>
+          <img
+            className="portfolio-image-popupbox"
+            src={a.name}
+            alt={a.desc}
+          />
+          <a className="hyper-link" onClick={() => window.open(a.link)}>
+            Click here to go to site
+          </a>
+        </>
+      );
+      PopupboxManager.open({ content });
+    };
+
+    return (
+      <div key={idx} className="image-box-wrapper row justify-content-center">
+        <div className="portfolio-image-box" onClick={openPopupbox}>
+          <img className="portfolio-image" src={a.name} alt={a.desc} />
+          <div className="overflow"></div>
+          <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div className="portfolio-wrapper">
       <div className="container">
         <h1 className="text-uppercase text-center py-5">Portfolio</h1>
-        {/* -  */}
-        <div className="image-box-wrapper row justify-content-center">
-          <div className="portfolio-image-box" onClick={openPopupbox}>
-            <img
-              className="portfolio-image"
-              src={map}
-              alt="google map clone project"
-            />
-            <div className="overflow"></div>
-            <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
-          </div>
-          {/* - */}
-          <div className="portfolio-image-box">
-            <img
-              className="portfolio-image"
-              src={calc}
-              alt="calculator project"
-            />
-            <div className="overflow"></div>
-            <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
-          </div>
-          {/* - */}
-          <div className="portfolio-image-box">
-            <img
-              className="portfolio-image"
-              src={drumkit}
-              alt="drumkit project"
-            />
-            <div className="overflow"></div>
-            <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
-          </div>
-          {/* - */}
-          <div className="portfolio-image-box">
-            <img
-              className="portfolio-image"
-              src={generativeArt}
-              alt="generative project"
-            />
-            <div className="overflow"></div>
-            <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
-          </div>
-          {/* - */}
-          <div className="portfolio-image-box">
-            <img
-              className="portfolio-image"
-              src={gorjeo}
-              alt="gorjeo project"
-            />
-            <div className="overflow"></div>
-            <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
-          </div>
-          {/* - */}
-          <div className="portfolio-image-box">
-            <img
-              className="portfolio-image"
-              src={interactiveparticles}
-              alt="interactiveparticles project"
-            />
-            <div className="overflow"></div>
-            <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
-          </div>
-          {/* - */}
-          <div className="portfolio-image-box">
-            <img
-              className="portfolio-image"
-              src={rememberSeq}
-              alt="rememberSeq project"
-            />
-            <div className="overflow"></div>
-            <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
-          </div>
-          {/* - */}
-          <div className="portfolio-image-box">
-            <img
-              className="portfolio-image"
-              src={spartikus}
-              alt="spartikus project"
-            />
-            <div className="overflow"></div>
-            <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
-          </div>
-          {/* - */}
-          <div className="portfolio-image-box">
-            <img
-              className="portfolio-image"
-              src={stooltracker}
-              alt="stooltracker project"
-            />
-            <div className="overflow"></div>
-            <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
-          </div>
-          {/* - */}
-          <div className="portfolio-image-box">
-            <img
-              className="portfolio-image"
-              src={tindog}
-              alt="tindog project"
-            />
-            <div className="overflow"></div>
-            <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
-          </div>
-          {/* - */}
-          <div className="portfolio-image-box">
-            <img className="portfolio-image" src={todos} alt="todos project" />
-            <div className="overflow"></div>
-            <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
-          </div>
-          {/* - */}
-          <div className="portfolio-image-box">
-            <img
-              className="portfolio-image"
-              src={weatherteller}
-              alt="weatherteller project"
-            />
-            <div className="overflow"></div>
-            <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
-          </div>
-        </div>
-        {/* - */}
+        {mappedProjects}
       </div>
       <PopupboxContainer {...popupboxConfig} />
     </div>
---
+++ src/projectsInfo/projectsInfo.js
@@ -0,0 +1,81 @@
+//add project on top for latest
+import map from "../image/projectsicon/map.png";
+import tindog from "../image/projectsicon/tindog.png";
+import rememberSeq from "../image/projectsicon/remember-seq.png";
+import drumkit from "../image/projectsicon/drumkit.png";
+import interactiveparticles from "../image/projectsicon/interactiveparticles.gif";
+import generativeArt from "../image/projectsicon/generative_art.png";
+import gorjeo from "../image/projectsicon/gorjeo3.jpg";
+import spartikus from "../image/projectsicon/spartikus.png";
+import stooltracker from "../image/projectsicon/stooltracker.jpg";
+import weatherteller from "../image/projectsicon/weatherteller.jpg";
+import todos from "../image/projectsicon/todos.png";
+import calc from "../image/projectsicon/calc.png";
+
+//add project on top for latest
+
+const projectsInfo = [
+  {
+    name:  map,
+    link: 'https://jcgooglemapsreplica.netlify.app/',
+    desc: 'Google map clone project'
+  },
+  {
+    name:  tindog,
+    link: 'https://tindogclone.netlify.app/',
+    desc: 'Tindog clone project'
+  },
+  {
+    name:  rememberSeq,
+    link: 'https://rememberthecolor.netlify.app/',
+    desc: 'Remember the sequence game'
+  },
+  {
+    name:  drumkit,
+    link: 'https://browserdrumkit.netlify.app/',
+    desc: 'Drumkit game'
+  },
+  {
+    name:  interactiveparticles,
+    link: 'https://interactiveparticles.netlify.app/',
+    desc: 'Interactive particles'
+  },
+  {
+    name:  generativeArt,
+    link: 'https://generativeart.netlify.app/',
+    desc: 'Generative Art project'
+  },
+  {
+    name:  gorjeo,
+    link: 'https://gorjeo.netlify.app/',
+    desc: 'Gorjeo project'
+  },
+  {
+    name:  spartikus,
+    link: 'https://sportikus.netlify.app/',
+    desc: 'Spartikus project'
+  },
+  {
+    name:  stooltracker,
+    link: 'https://stooltracker.netlify.app/',
+    desc: 'Stool Tracker project'
+  },
+  {
+    name:  weatherteller,
+    link: 'https://theweatherteller.netlify.app/',
+    desc: 'Weather Teller project'
+  },
+  {
+    name:  todos,
+    link: 'https://juancamachotodoapp.netlify.app/',
+    desc: 'Todos project'
+  },
+  {
+    name:  calc,
+    link: 'https://calculator-jc.netlify.app/',
+    desc: 'Calculator project'
+  },
+  
+]
+  
+export default projectsInfo;
