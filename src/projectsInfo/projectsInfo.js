4d6c9fc2703f4dba685b4b7567fc5001e093170b
juan camacho <juanc@tiv.solutions>
2021-09-20 20:27:55 -0400

    norefferer

--- src/projectsInfo/projectsInfo.js
+++ src/projectsInfo/projectsInfo.js
@@ -1,6 +1,6 @@
 //add project on top for latest
-import chat from '../image/projectsicon/9chat.gif'
-import touringNat from '../image/projectsicon/touringnat.gif';
+import chat from "../image/projectsicon/9chat.gif";
+import touringNat from "../image/projectsicon/touringnat.gif";
 import portfoliov1 from "../image/projectsicon/portfoliov1.JPG";
 import map from "../image/projectsicon/map.png";
 import tindog from "../image/projectsicon/tindog.png";
@@ -14,148 +14,155 @@ import stooltracker from "../image/projectsicon/stooltracker.jpg";
 import weatherteller from "../image/projectsicon/weatherteller.jpg";
 import todos from "../image/projectsicon/todos.png";
 import calc from "../image/projectsicon/calc.png";
-import udemyPortfolio from '../image/projectsicon/udemyPortfolio.png'
-import naujcommerce from '../image/projectsicon/naujcommerce.png'
-import decomelos from '../image/projectsicon/decomelos.png'
-import tictactie from '../image/projectsicon/tictactoe.png'
+import udemyPortfolio from "../image/projectsicon/udemyPortfolio.png";
+import naujcommerce from "../image/projectsicon/naujcommerce.png";
+import decomelos from "../image/projectsicon/decomelos.png";
+import tictactie from "../image/projectsicon/tictactoe.png";
 
 //add project on top for latest
 
 const projectsInfo = [
   {
-    name:  naujcommerce,
-    link: 'https://naujcommerce.netlify.app/',
-    desc: 'Nauj Commerce',
-    summary: 'Just an e-commerce site',
-    summary2: 'React'
+    name: naujcommerce,
+    link: "https://naujcommerce.netlify.app/",
+    desc: "Nauj Commerce",
+    summary: "Just an e-commerce site",
+    summary2: "React",
   },
   {
-    name:  udemyPortfolio,
-    link: 'https://juancamachoudemyportfolio.netlify.app/',
-    desc: 'Tutorial I made at udemy.com',
-    summary: <a href='https://www.udemy.com/course/build-a-portfolio-with-reactjs/' target="_blank">https://www.udemy.com/course/build-a-portfolio-with-reactjs/</a>,
-    summary2: 'React'
+    name: udemyPortfolio,
+    link: "https://juancamachoudemyportfolio.netlify.app/",
+    desc: "Tutorial I made at udemy.com",
+    summary: (
+      <a
+        href="https://www.udemy.com/course/build-a-portfolio-with-reactjs/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        https://www.udemy.com/course/build-a-portfolio-with-reactjs/
+      </a>
+    ),
+    summary2: "React",
   },
   {
-    name:  tictactie,
-    link: 'https://jctic-tac-toe.netlify.app/',
-    desc: 'A game..',
-    summary: 'called tic tac toe',
-    summary2: 'React'
+    name: tictactie,
+    link: "https://jctic-tac-toe.netlify.app/",
+    desc: "A game..",
+    summary: "called tic tac toe",
+    summary2: "React",
   },
   {
-    name:  decomelos,
-    link: 'https://decomelos.netlify.app/',
-    desc: 'Page for a client',
-    summary: 'Page for a client',
-    summary2: 'React'
+    name: decomelos,
+    link: "https://decomelos.netlify.app/",
+    desc: "Page for a client",
+    summary: "Page for a client",
+    summary2: "React",
   },
   {
-    name:  chat,
-    link: 'https://chat-app-juancamacho.netlify.app/',
-    desc: '9 Chat',
-    summary: 'An app to chat using Firebase as a databse',
-    summary2: 'React'
+    name: chat,
+    link: "https://chat-app-juancamacho.netlify.app/",
+    desc: "9 Chat",
+    summary: "An app to chat using Firebase as a databse",
+    summary2: "React",
   },
   {
-    name:  touringNat,
-    link: 'https://touringnat.netlify.app/',
-    desc: 'Touring Nat',
-    summary: 'The powers of CSS',
-    summary2: 'HTML | CSS | SASS'
+    name: touringNat,
+    link: "https://touringnat.netlify.app/",
+    desc: "Touring Nat",
+    summary: "The powers of CSS",
+    summary2: "HTML | CSS | SASS",
   },
   {
-    name:  portfoliov1,
-    link: 'https://juancamachoportfolio-first.netlify.app/',
-    desc: 'First Version of my portfolio',
-    summary: 'When I graduated from General Assembly School',
-    summary2: 'HTML | CSS | JavaScript | Bootstrap'
+    name: portfoliov1,
+    link: "https://juancamachoportfolio-first.netlify.app/",
+    desc: "First Version of my portfolio",
+    summary: "When I graduated from General Assembly School",
+    summary2: "HTML | CSS | JavaScript | Bootstrap",
   },
   {
-    name:  map,
-    link: 'https://jcgooglemapsreplica.netlify.app/',
-    desc: 'Google map clone project',
-    summary: 'This app recreates a functional GPS system',
-    summary2: 'JavaScript | CSS | HTML'
+    name: map,
+    link: "https://jcgooglemapsreplica.netlify.app/",
+    desc: "Google map clone project",
+    summary: "This app recreates a functional GPS system",
+    summary2: "JavaScript | CSS | HTML",
   },
   {
-    name:  tindog,
-    link: 'https://tindogclone.netlify.app/',
-    desc: 'Tindog clone project',
-    summary: 'Tinder for dogs',
-    summary2: 'HTML | CSS | JavaScript | Bootstrap'
+    name: tindog,
+    link: "https://tindogclone.netlify.app/",
+    desc: "Tindog clone project",
+    summary: "Tinder for dogs",
+    summary2: "HTML | CSS | JavaScript | Bootstrap",
   },
   {
-    name:  rememberSeq,
-    link: 'https://rememberthecolor.netlify.app/',
-    desc: 'Remember the sequence game',
-    summary: 'remember the sequence of the colors, with sounds too',
-    summary2: 'HTML | CSS | JavaScript'
+    name: rememberSeq,
+    link: "https://rememberthecolor.netlify.app/",
+    desc: "Remember the sequence game",
+    summary: "remember the sequence of the colors, with sounds too",
+    summary2: "HTML | CSS | JavaScript",
   },
   {
-    name:  drumkit,
-    link: 'https://browserdrumkit.netlify.app/',
-    desc: 'Drumkit game',
-    summary:'make some beats!',
-    summary2: 'HTML | CSS | JavaScript'
+    name: drumkit,
+    link: "https://browserdrumkit.netlify.app/",
+    desc: "Drumkit game",
+    summary: "make some beats!",
+    summary2: "HTML | CSS | JavaScript",
   },
   {
-    name:  interactiveparticles,
-    link: 'https://interactiveparticles.netlify.app/',
-    desc: 'Interactive particles',
-    summary: 'this is just a cool effect when hovering with your mouse',
-    summary2: 'HTML | CSS | JavaScript'
+    name: interactiveparticles,
+    link: "https://interactiveparticles.netlify.app/",
+    desc: "Interactive particles",
+    summary: "this is just a cool effect when hovering with your mouse",
+    summary2: "HTML | CSS | JavaScript",
   },
   {
-    name:  generativeArt,
-    link: 'https://generativeart.netlify.app/',
-    desc: 'Generative Art project',
-    summary: 'click and drag to draw whatever you want',
-    summary2: 'HTML | CSS | JavaScript'
+    name: generativeArt,
+    link: "https://generativeart.netlify.app/",
+    desc: "Generative Art project",
+    summary: "click and drag to draw whatever you want",
+    summary2: "HTML | CSS | JavaScript",
   },
   {
-    name:  gorjeo,
-    link: 'https://gorjeo.netlify.app/',
-    desc: 'Gorjeo project',
-    summary: 'last project for the bootcamp at General Assembly',
-    summary2: 'React | Ruby | Rails | CSS | PostgreSQL'
+    name: gorjeo,
+    link: "https://gorjeo.netlify.app/",
+    desc: "Gorjeo project",
+    summary: "last project for the bootcamp at General Assembly",
+    summary2: "React | Ruby | Rails | CSS | PostgreSQL",
   },
   {
-    name:  spartikus,
-    link: 'https://sportikus.netlify.app/',
-    desc: 'Spartikus project',
-    summary: 'third project for the bootcamp at General Assembly',
-    summary2: 'React | Mongodb'
+    name: spartikus,
+    link: "https://sportikus.netlify.app/",
+    desc: "Spartikus project",
+    summary: "third project for the bootcamp at General Assembly",
+    summary2: "React | Mongodb",
   },
   {
-    name:  stooltracker,
-    link: 'https://stooltracker.netlify.app/',
-    desc: 'Stool Tracker project',
-    summary: 'second project for the bootcamp at General Assembly',
-    summary2: 'React | Airtable DB'
+    name: stooltracker,
+    link: "https://stooltracker.netlify.app/",
+    desc: "Stool Tracker project",
+    summary: "second project for the bootcamp at General Assembly",
+    summary2: "React | Airtable DB",
   },
   {
-    name:  weatherteller,
-    link: 'https://theweatherteller.netlify.app/',
-    desc: 'Weather Teller project',
-    summary: 'my very first project for the bootcamp at General Assembly',
-    summary2: 'HTML | CSS | JavaScript'
+    name: weatherteller,
+    link: "https://theweatherteller.netlify.app/",
+    desc: "Weather Teller project",
+    summary: "my very first project for the bootcamp at General Assembly",
+    summary2: "HTML | CSS | JavaScript",
   },
   {
-    name:  todos,
-    link: 'https://juancamachotodoapp.netlify.app/',
-    desc: 'Todos project',
-    summary: 'second side project to train skills',
-    summary2: 'HTML | CSS | JavaScript'
+    name: todos,
+    link: "https://juancamachotodoapp.netlify.app/",
+    desc: "Todos project",
+    summary: "second side project to train skills",
+    summary2: "HTML | CSS | JavaScript",
   },
   {
-    name:  calc,
-    link: 'https://calculator-jc.netlify.app/',
-    desc: 'Calculator project',
-    summary: 'first side project to train my skills',
-    summary2: 'HTML | CSS | JavaScript'
+    name: calc,
+    link: "https://calculator-jc.netlify.app/",
+    desc: "Calculator project",
+    summary: "first side project to train my skills",
+    summary2: "HTML | CSS | JavaScript",
   },
-  
-]
-  
+];
+
 export default projectsInfo;
