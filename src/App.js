97d8b81ce85f340fd5485decb9821e4167156e93
juan camacho <juanc@tiv.solutions>
2022-01-22 13:35:47 -0500

    removed ts particles

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css"
-import Particles from 'react-particles-js';
-import Navbar from './components/Navbar';
-import Header from './components/Header';
-import AboutMe from './components/AboutMe';
-import Services from './components/Services';
-import Experience from './components/Experience';
-import Portfolio from './components/Portfolio';
-import Contacts from './components/Contacts';
-import Footer from './components/Footer';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import Particles from 'react-particles-js';
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import AboutMe from "./components/AboutMe";
+import Services from "./components/Services";
+import Experience from "./components/Experience";
+import Portfolio from "./components/Portfolio";
+import Contacts from "./components/Contacts";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <Particles 
+      {/* <Particles 
       params={{particles: {
         number: {
           value: 30,
@@ -30,7 +30,7 @@ function App() {
           }
         }
       }}}
-      />
+      /> */}
       <Navbar />
       <Header />
       <AboutMe />
